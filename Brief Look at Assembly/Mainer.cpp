9a6602d0a1e813e6d2149eb0b83d0f200bb28099
Kulpot <kulpotwahu@gmail.com>
2023-10-06 14:47:21 +0800

    Add project files.

---
+++ Brief Look at Assembly/Mainer.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+//ref link:https://www.youtube.com/watch?v=Gna5eoJ0mhw&list=PLRwVmtr-pp05c1HTBj1no6Fl6C6mlxYDG&index=6
+//
+
+void main()
+{
+	for (int i = 0; i < 10; i++)
+	{
+		cout << i << endl;
+	}
+}
